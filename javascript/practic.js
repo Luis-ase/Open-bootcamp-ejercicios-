98ad49b6fe4e53a56179af2e2b9d8afe7b12b983
Luis Condori <luis2003nb@gmail.com>
2023-01-04 22:07:22 -0300

    first commit

---
+++ javascript/practic.js
@@ -0,0 +1,13 @@
+//number
+
+4
+//string
+"hola mundo"
+`como estas`
+
+//booleanos
+true
+false
+//null and undefined
+null
+undefined
---
+++ javascript/variable.js
@@ -0,0 +1,62 @@
+var variable;
+let variablelet;
+
+
+const variableconstante="";
+
+var a =1;
+console.log(a);
+
+
+const cositab =4;
+ 
+
+// []=> array/ lista 
+const array=[1,2,3,4]
+//{}=> llaves/ para objetos,funciones, y estructuras de control
+const objetoss={
+
+    name:"hola",
+    apellido:"cosita"
+}
+//()b => parentesis/ se utiliza para hacer funciones 
+function ejemplo(hola){
+//es un cualquier tipo de dato es un argumento
+}
+if(cositab===4){
+    let nuevo="soy nuevo"
+}
+// 
+
+// 
+// 
+
+// 
+console.log(cositab)
+
+
+//        Listas,Objetos,  y fechas en js
+
+
+// listas
+
+const lista= [1,true,false,2,"hola",["como estas","hola"],{comida:"pasta con tomate"}]
+
+// objetos
+
+const ejemploObjeto={
+    precio:2,
+    altura:14,
+    ancho:4,
+    maraca:"xiaomi",
+    iswhite:false,
+    contac:["Gorilla","open","Bootcamp"],
+
+}
+// fechas
+
+//librerias de apoyo Moment.js
+
+let cosita=new Date()
+
+console.log(cosita)
